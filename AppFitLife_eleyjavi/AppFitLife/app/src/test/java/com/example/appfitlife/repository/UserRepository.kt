e8290d6a621ad5ddef9ae2fdcbc791fffcb5e4cf
EAikeE <166560895+EAikeE@users.noreply.github.com>
2025-09-29 14:13:00 -0300

    Delete AppFitLife_eleyjavi directory

--- AppFitLife_eleyjavi/AppFitLife/app/src/test/java/com/example/appfitlife/repository/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.appfitlife.repository
-
-import com.example.appfitlife.model.User
-
-class UserRepository {
-    fun getUser(): User {
-        return User(1, "Javiera Marchesse y Elena Espinoza", "ami@appfitlife.com")
-    }
-}
